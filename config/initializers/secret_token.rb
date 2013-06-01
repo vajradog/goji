c0bd8561c546398a3134f8aa1823452e69cbfb80
vajradog <vajradog@gmail.com>
2013-06-01 14:01:36 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goji::Application.config.secret_token = '04e657e70eece2774695d2f8f811729adc49c6d37ddc17295bd1b0cdce48017610f26a4ccbb3b63dc9a709302e79da854e47ca5fbc9d512ebfebc569223261c6'
